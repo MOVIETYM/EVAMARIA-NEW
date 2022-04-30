4a712d9352f6e4028685bdaf1ef565fa9756dced
ATHOLOKAM <91723584+ATHOLOKAM@users.noreply.github.com>
2022-04-30 12:15:14 +0530

    Update details.py

--- plugins/lallu_tg/details.py
+++ plugins/lallu_tg/details.py
@@ -1,1 +1,8 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
 
+
+@Client.on_message(filters.command("details"))
+async def details(bot, message): 
+    await message.reply_text(
+        text=f"""
