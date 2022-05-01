e445965eb7b202dbb356602bd224246a8628b753
ATHOLOKAM <91723584+ATHOLOKAM@users.noreply.github.com>
2022-05-01 20:58:15 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -2,6 +2,7 @@ import os
 import logging
 import random
 import asyncio
+import script
 from pyrogram import Client, filters
 from pyrogram.errors import ChatAdminRequired, FloodWait
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
