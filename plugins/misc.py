f6ddd7778992652562f486f3dbe7581d3eb72c70
ATHOLOKAM <91723584+ATHOLOKAM@users.noreply.github.com>
2022-05-02 09:09:08 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -6,6 +6,7 @@ from info import IMDB_TEMPLATE
 from utils import extract_user, get_file_id, get_poster, last_online
 import time
 import random
+from Script import script
 from datetime import datetime
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 import logging
