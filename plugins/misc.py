4ace844c41edac1f37428f570354a953050a824d
ATHOLOKAM <91723584+ATHOLOKAM@users.noreply.github.com>
2022-05-02 09:13:43 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -76,7 +76,7 @@ async def aboutme(client, message):
         )
 
 @Client.on_message(filters.command(["info"]))
-async def who_is(client, message):
+async def who(client, message):
     # https://github.com/SpEcHiDe/PyroGramBot/blob/master/pyrobot/plugins/admemes/whois.py#L19
     status_message = await message.reply_text(
         "`𝚂𝙴𝙰𝚁𝙲𝙷𝙸𝙽𝙶 𝚄𝚂𝙴𝚁...`"
